369ac413639c70cb57f0882e3ec123564bceca8d
Neo <142568339+neeow@users.noreply.github.com>
2023-08-23 15:35:14 +0200

    Add files via upload

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_print_reverse_alphabet.c                        :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nchan <marvin@codam.nl>                      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/08/21 14:00:31 by nchan         #+#    #+#                 */
+/*   Updated: 2023/08/22 19:09:01 by nchan         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
+}
+/*
+int	main(void)
+{
+	ft_print_reverse_alphabet();
+	return (0);
+}
+*/
---
+++ C00/ex04/ft_is_negative.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_is_negative.c                                   :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nchan <marvin@codam.nl>                      +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/08/22 16:40:35 by nchan         #+#    #+#                 */
+/*   Updated: 2023/08/22 19:10:41 by nchan         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+//#include <unistd.h>
+
+void	ft_is_negative(int n)
+{
+	if (n >= 0)
+		write (1, &"P", 1);
+	else
+		write (1, &"N", 1);
+}
+/*
+int	main(void)
+{
+	ft_is_negative(-1);
+	ft_is_negative(0);
+	ft_is_negative(1);
+	return (0);
+}
+*/
