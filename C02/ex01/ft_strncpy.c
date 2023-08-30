0fb1a2f48f7b3f35e816ff67409b86df11180ae1
Neo <142568339+neeow@users.noreply.github.com>
2023-08-30 18:19:37 +0200

    Add files via upload

---
+++ C02/ex01/ft_strncpy.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_strncpy.c                                       :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nchan <nchan@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/08/29 14:48:37 by nchan         #+#    #+#                 */
+/*   Updated: 2023/08/30 16:15:31 by nchan         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <string.h>
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
+
+int	main()
+{
+	char src[] = "noError";
+	char dest[] = "code69";
+	char dest1[] = "codam";
+
+	printf("%s", ft_strncpy(dest, src, 3));
+	printf("\n%s", ft_strncpy(dest1, src, 5));
+}
