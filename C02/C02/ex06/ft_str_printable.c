318e9b780dc5b8b0c702695190670800fc302dd1
Neo <142568339+neeow@users.noreply.github.com>
2023-08-31 17:55:18 +0200

    Add files via upload

---
+++ C02/C02/ex06/ft_str_printable.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_str_printable.c                                 :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: nchan <nchan@student.codam.nl>               +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/08/31 17:46:51 by nchan         #+#    #+#                 */
+/*   Updated: 2023/08/31 17:49:33 by nchan         ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_printable(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if ((str[i] < 32 || str [i] > 126))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+/*
+int	main(void)
+{
+	printf("%d", ft_str_printable("abc"));
+	printf("\n%d", ft_str_printable("€"));
+	printf("\n%d", ft_str_printable("\n"));
+}
+*/
